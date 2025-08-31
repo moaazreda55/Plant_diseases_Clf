61f21f4f5f136d53976887d813d88326fbc445d0
moaazreda55 <111019977+moaazreda55@users.noreply.github.com>
2025-08-31 22:49:43 +0300

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,13 +1,14 @@
 import gdown
 import os
 
-os.makedirs("models",exist_ok=True)
+os.makedirs("models", exist_ok=True)
 
 
-def download_model(file_id,name):
+def download_model(file_id, name):
     
     url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url,f"models/{name}")
+    gdown.download(url, f"models/{name}")
 
 
-download_model("165IPUEwUbukYadd-0nkRMUlML9H4mp0p","plant.pth")
+download_model("165IPUEwUbukYadd-0nkRMUlML9H4mp0p", "plant.pth")
+
